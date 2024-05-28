c6f14c475b2f7aabcceed9bf36da3ed31d6e5b0f
vitor hugo <vitorhugo85670@gmail.com>
2024-05-28 18:51:26 -0300

    validação do fomrulario de perfil individudal

---
+++ codigo-fonte/assets/JS/individual.js
@@ -0,0 +1,69 @@
+// armazenar os dados digitados nos campos em constatantes
+const dadosIndividual = document.querySelector('#dados_usuario');
+const nome = document.querySelector('#nome');
+const sobrenome = document.querySelector('#sobrenome');
+const tel = document.querySelector('#number');
+const dataNascimento = document.querySelector('#DataNascimento');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+
+dadosIndividual.addEventListener("submit", (event) => {
+    event.preventDefault();
+// validar as  informaçoes
+    if (nome.value === "") {
+        alert("Por favor, preencha seu nome.");
+        return;
+    }
+
+    if (sobrenome.value === "") {
+        alert("Por favor, preencha seu sobrenome.");
+        return;
+    }
+
+    if (tel.value === "" || !TelValidado(tel.value)) {
+        alert("Por favor, preencha com um telefone válido.");
+        return;
+    }
+
+    if (email.value === "" || !EmailValidado(email.value)) {
+        alert("Por favor, preencha com um email válido.");
+        return;
+    }
+
+    if (!ValidateSenha(senha.value, 8)) {
+        alert("A senha precisa ter no mínimo 8 caracteres.");
+        return;
+    }
+
+        // Armazenar os dados validados na localStorage
+        const dados = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            telefone: tel.value,
+            email: email.value
+            // Adicione mais campos, se necessário
+        };
+        localStorage.setItem('dadosUsuario', JSON.stringify(dados));
+    
+        // Enviar o formulário
+        dadosIndividual.submit();
+        alert("dados enviados com sucesso")
+         
+
+});
+
+function TelValidado(tel) {
+    const telRegex = /^\(?\+?(\d{1,3})?\)?[-.\s]?(\d{2,4})[-.\s]?(\d{4,5})[-.\s]?(\d{4})$/;
+    return telRegex.test(tel);
+}
+
+function EmailValidado(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+function ValidateSenha(senha, minimoDigitos) {
+    return senha.length >= minimoDigitos;
+}
+
+
