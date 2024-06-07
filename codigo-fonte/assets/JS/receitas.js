1109a4301cdfa55d14705b70383b5ba633471d2a
marcellapontes1 <marcellapontesdosanjos@hotmail.com>
2024-06-06 21:17:45 -0300

    atualizando js

--- codigo-fonte/assets/JS/receitas.js
+++ codigo-fonte/assets/JS/receitas.js
@@ -33,23 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function loadMembers(){
-    const usuarios = document.getElementById('usuario');
-    const profile = JSON.parse(localStorage.getItem('user_profile'))
-    const members = profile['members']
-
-    console.log(`${profile} / ${members}`)
-    for(let idx in members){
-        const opt = document.createElement("option")
-        const member = members[idx]
-
-        opt.textContent = member.name;
-        opt.value = member.name;
-
-        usuarios.appendChild(opt)
-    }
-}
-
 function checkUserState(){
     const userName = localStorage.getItem('name');
     const userEmail = localStorage.getItem('email')
