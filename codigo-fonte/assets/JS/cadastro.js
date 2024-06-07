4874d33b2eba3d6eaf52e70820239c02e5bc7c5b
marcellapontes1 <marcellapontesdosanjos@hotmail.com>
2024-06-06 21:00:13 -0300

    atualizando

--- codigo-fonte/assets/JS/cadastro.js
+++ codigo-fonte/assets/JS/cadastro.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const cadastrarBtn = document.getElementById('cadastrar');
     const entrarBtn = document.getElementById('entrar');
-    const formulario = document.getElementById('seuFormularioId'); 
+
     cadastrarBtn.addEventListener('click', () => {
         const nome = document.getElementById('nome').value;
         const email = document.getElementById('email').value;
@@ -9,22 +9,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         if (nome && email && senha) {
             if (senha.length >= 6 && /[A-Z]/.test(senha)) {
-                
                 console.log('Nome:', nome);
                 console.log('Email:', email);
                 console.log('Senha:', senha);
-                localStorage.setItem("name", `${nome}`);
-                localStorage.setItem("email", `${email}`);
-                localStorage.setItem("senha", `${senha}`);
+                localStorage.setItem("name", nome);
+                localStorage.setItem("email", email);
+                localStorage.setItem("senha", senha);
                 alert('Cadastro realizado com sucesso!');
-                
-                window.location.href = "cadastro_perfil.html"; // Redireciona para a página de perfil após o cadastro
+                window.location.href = "cadastro_perfil.html";
             } else {
                 alert('A senha deve ter pelo menos 6 caracteres e incluir uma letra maiúscula.');
             }
         } else {
             alert('Por favor, preencha todos os campos.');
         }
+    });
+
+    entrarBtn.addEventListener('click', () => {
+        window.location.href = "login.html";
     });
 });
 
