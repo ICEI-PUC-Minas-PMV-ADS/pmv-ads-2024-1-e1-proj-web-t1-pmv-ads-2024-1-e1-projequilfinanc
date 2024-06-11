f43c5247b26fbb6b0be97fafbbd5ac025189a205
marcellapontes1 <marcellapontesdosanjos@hotmail.com>
2024-06-11 20:55:30 -0300

    atualizando pastas

---
+++ codigo-fonte/assets/base.js
@@ -0,0 +1,26 @@
+/* document.addEventListener('DOMContentLoaded', (event) => {
+    loadMembers()
+})
+
+function toProfile(){
+    console.log('called')
+    const profile = JSON.parse(localStorage.getItem('user_profile'))
+    document.location.href = profile.profile == 1 ? 'Individual.html' : 'Familiar.html'
+}
+
+function loadMembers(){
+    const usuarios = document.getElementById('usuario');
+    const profile = JSON.parse(localStorage.getItem('user_profile'))
+    const members = profile['members']
+
+    console.log(`${profile} / ${members}`)
+    for(let idx in members){
+        const opt = document.createElement("option")
+        const member = members[idx]
+
+        opt.textContent = member.name;
+        opt.value = member.name;
+
+        usuarios.appendChild(opt)
+    }
+} */
