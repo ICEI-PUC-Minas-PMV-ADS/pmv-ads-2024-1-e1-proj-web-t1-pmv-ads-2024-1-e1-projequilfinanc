ae7fbef5096e1d61d4f66cd90e73cc5458f2d565
marcellapontes1 <marcellapontesdosanjos@hotmail.com>
2024-06-11 21:11:46 -0300

    atualizando js

---
+++ codigo-fonte/pagina-principal/base.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadMembers()
+})
+
+function toProfile(){
+    console.log('called')
+    const profile = JSON.parse(localStorage.getItem('esg_pincho1'))
+    document.location.href = profile.profile == 1 ? '/codigo-fonte/pagina-familiar/Familiar.html' : '/codigo-fonte/pagina-individual/Individual.html'
+}
+
+function loadMembers(){
+    const usuarios = document.getElementById('usuario');
+    const profile = JSON.parse(localStorage.getItem('esg_pincho1'))
+    const members = profile['members']
+
+    console.log(`${profile} / ${members}`)
+    for(let idx in members){
+        const opt = document.createElement("option")
+        const member = members[idx]
+
+        opt.textContent = member.name;
+        opt.value = member.name;
+
+        usuarios.appendChild(opt)
+    }
+} 
